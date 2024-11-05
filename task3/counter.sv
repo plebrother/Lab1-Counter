fa753efb23163746f3083d4f2255cd78130aa7dd
Yikai Su <62829237+plebrother@users.noreply.github.com>
2024-11-05 11:21:11 +0000

    update task3

---
+++ task3/counter.sv
@@ -0,0 +1,17 @@
+module counter #(
+  parameter WIDTH = 8
+)(
+  // interface signals
+  input  logic             clk,      // clock
+  input  logic             rst,      // reset
+  input  logic             ld,       // load counter from data
+  input  logic             en,
+  input  logic [WIDTH-1:0] v,        // value to preload
+  output logic [WIDTH-1:0] count     // count output
+);
+
+always_ff @ (posedge clk)
+  if (rst) count <= {WIDTH{1'b0}};
+  else count <= count + {{WIDTH-1{1'b0}},en};
+
+endmodule
