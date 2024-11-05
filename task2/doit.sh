37cbfd57a2ff7d0eddad61187dd44bf2684a7f77
Yikai Su <62829237+plebrother@users.noreply.github.com>
2024-11-05 10:54:38 +0000

    update
    update

---
+++ task2/doit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+~/Documents/iac/lab0-devtools/tools/attach_usb.sh
+
+rm -rf obj_dir
+rm -f counter.vcd
+
+verilator -Wall --cc --trace counter.sv --exe counter_tb.cpp
+
+make -j -C obj_dir/ -f Vcounter.mk Vcounter
+
+obj_dir/Vcounter
